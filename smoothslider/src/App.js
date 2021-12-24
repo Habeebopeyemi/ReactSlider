69af2eb779141ce7b245f429310560dbbcc923ec
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2021-12-24 19:44:14 +0100

    setup working directory and component

---
+++ smoothslider/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>am happy with emiola</h1>
+    </div>
+  );
+}
+
+export default App;
